d73664c0bf5f2f7bce47d2e15168e7f84cb51820
ildanga <cjiyoon0418@naver.com>
2023-03-15 13:28:43 +0900

    Master 2

--- main.py
+++ main.py
@@ -12,10 +12,14 @@ dst1, dst2, dst3 = None, None, None # 영상처리한 결과
 ##메인 코드부
 src = cv2.imread('c:/images/picture22.jpg') # 이미지 읽기
 
+dst1 = grayScale(src)
+dst2 = cannyEdgy(src)
 
 
 
 cv2.imshow('Src', src) # 화면 출력
+cv2.imshow('Dst1', dst1)
+cv2.imshow('Dst2', dst2)
 
 ##마무리
 cv2.waitKey(0)
